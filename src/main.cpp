562958e9db8a7b72764d5441c620bf1885e99556
unknown <thomaskuwaikwan@gmail.com>
2024-10-03 00:54:28 +0800

    fixed wakeup bug

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 #define lightPin 4
 
-int usFactor = 1000000;  //us to s multiplier         
-int detectGap = 5;       //time between checking of light sensor values
-bool espState = true;    //set ESP to be On for default
+int usFactor = 1000000;   //us to s multiplier         
+int detectGap = 5;        //time between checking of light sensor values
+bool espState = false;    //set ESP to be Off for default
+int count = 0;            //Resolve wake up bug: light becoming 4095 everytime after wake up
 
 void powerDown()
 {
@@ -24,16 +25,20 @@ void powerUp()
 
 void setup()
 {
-  Serial.begin(9600);             //set up serial communication
+  Serial.begin(19200);             //set up serial communication
   pinMode(lightPin, INPUT);       //configure photoresistor pin
+  esp_sleep_enable_timer_wakeup(detectGap * usFactor);  //wake up to do lightLevel checking at a regular interval 
 }
 
 void loop()
-{
-  int light = analogRead(lightPin);                     //read the photoresistor module value
-  int lightLevel = map(light, 0, 4095, 0, 15);          //maping the lightLevel into 16 values
-  esp_sleep_enable_timer_wakeup(detectGap * usFactor);  //wake up to do lightLevel checking at a regular interval 
-  
+{ 
+
+  int light1 = analogRead(lightPin);                     //read the photoresistor module value
+  int light2 = analogRead(lightPin);
+  int avgLight = (light1 + light2)/2;
+  int lightLevel = map(avgLight, 0, 4095, 0, 15);          //maping the lightLevel into 16 values
+  Serial.println(avgLight);
+  delay(1000);
 //lightLevel Checks
 
   //if light level is low: Go to deep sleep mode
@@ -43,9 +48,11 @@ void loop()
   }
 
   //if the ESP is "off" and light level is high: Do not go back to deep sleep mode
-  else if (lightLevel > 11 && !espState)
+  //resolved problem: will not run powerUp() by the wake up bug
+  else if (lightLevel > 11 && !espState && count)
   {
     powerUp();
   }
+ count++;
  
 }
